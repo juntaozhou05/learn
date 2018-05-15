e56d13b5fbff3a10ff04f498af0fd49952a8fd92
juntaozhou05 <350529307@qq.com>
2018-05-15 16:36:04 +0800

    add todo-list

--- js/todoList.js
+++ js/todoList.js
@@ -1,5 +1,13 @@
-for (let i = 0; i < document.getElementsByTagName("li").length; i++) {
-  document.getElementsByTagName("li")[i].onclick = function(e) {
-    location.href = "./todeTask.html?id=" + (i + 1);
-  };
-}
+document.getElementById("task1").onclick = function() {
+  if (this.getAttribute("value") == 1) {
+    document.getElementById("list").style.display = "block";
+    document.getElementsByClassName("back")[0].style.display = "block";
+    document.getElementById("main").style.display = "none";
+  }
+};
+
+document.getElementsByClassName("back")[0].onclick = function() {
+  document.getElementById("list").style.display = "none";
+  document.getElementById("main").style.display = "block";
+  this.style.display = "none";
+};
