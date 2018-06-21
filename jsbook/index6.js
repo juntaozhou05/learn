086fe56711091de004fec427600a8ecacb8c96fd
juntaozhou05 <350529307@qq.com>
2018-06-21 11:22:00 +0800

    add methods

--- jsbook/index6.js
+++ jsbook/index6.js
@@ -48,3 +48,15 @@
 //   };
 // })();
 // console.log(proxyMult(1, 2, 3, 4));
+
+/*缓存代理工厂*/
+let createProxyFactory = function(fn) {
+  let cache = {};
+  return function() {
+    let args = Array.prototype.join.call(arguments, ",");
+    if (args in cache) {
+      return cache[args];
+    }
+    return (cache[args] = fn.apply(this.arguments));
+  };
+};
