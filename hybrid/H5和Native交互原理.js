a64a5630b41a5151bf4d69287c940c90cb75fa89
juntaozhou05 <350529307@qq.com>
2018-09-22 17:43:26 +0800

    add hybird files

--- hybrid/H5和Native交互原理.js
+++ hybrid/H5和Native交互原理.js
@@ -16,3 +16,44 @@
 // webSettings.setJavaScriptEnabled(true);
 // // Android容器设置侨连对象
 // mWebView.addJavascriptInterface(getJSBridge(), "JSBridge");
+
+// 5.然后H5中即可调用原生中注册的函数
+// // 调用方法一
+// window.JSBridge.foo(); // 返回:'foo'
+// // 调用方法二
+// window.JSBridge.foo2('test'); // 返回:'foo2:test'
+
+//6.H5调iOS：
+// 以OC为例
+
+// 首先，需要引入JavaScriptCore库
+
+// #import <JavaScriptCore/JavaScriptCore.h>
+// 然后原生需要注册API
+
+// //webview加载完毕后设置一些js接口
+// -(void)webViewDidFinishLoad:(UIWebView *)webView{
+//     [self hideProgress];
+//     [self setJSInterface];
+// }
+
+// -(void)setJSInterface{
+
+//     JSContext *context =[_wv valueForKeyPath:@"documentView.webView.mainFrame.javaScriptContext"];
+
+//     // 注册名为foo的api方法
+//     context[@"foo"] = ^() {
+
+//         //获取参数
+//         NSArray *args = [JSContext currentArguments];
+//         NSString *title = [NSString stringWithFormat:@"%@",[args objectAtIndex:0]];
+//         //做一些自己的逻辑
+//         //返回一个值  'foo:'+title
+//         return [NSString stringWithFormat:@"foo:%@", title];
+//     };
+
+// }
+// 之后前端就可以调用了
+
+// // 调用方法,用top是确保调用到最顶级,因为iframe要用top才能拿到顶级
+// window.top.foo('test'); // 返回:'foo:test'
---
+++ hybrid/JSBridge的原理.js
@@ -0,0 +1,2 @@
+//1.JSBridge是Native代码与JS代码的通信桥梁。目前的一种统一方案是:H5触发url scheme->Native捕获url scheme->原生分析,执行->原生调用h5。
+//2.
