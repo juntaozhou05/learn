8b72a5843816b2f010a48bec9d00a4c040d22e00
juntaozhou05 <350529307@qq.com>
2018-10-17 17:46:19 +0800

    add css

--- cssLearn/css.js
+++ cssLearn/css.js
@@ -1,4 +1,11 @@
-//超出文本省略号
+//1.超出文本省略号
 // overflow:hidden;
 // text-overflow:ellipsis;
 // white-space:nowrap
+
+//2.改变滚动
+//::-webkit-scrollbar
+// {
+//     width: 16px;  /*滚动条宽度*/
+//     height: 16px;  /*滚动条高度*/
+// }
