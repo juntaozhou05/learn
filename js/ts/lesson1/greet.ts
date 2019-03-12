e0f34b2af7a71c026c3909ee91d9b4f743ac13d6
juntaozhou05 <350529307@qq.com>
2019-03-12 22:34:14 +0800

    add ts

---
+++ js/ts/lesson1/greet.ts
@@ -0,0 +1,19 @@
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+class Student {
+  fullName!: string;
+  constructor(public firstName: string, _lastName: string) {
+    this.fullName = firstName + _lastName;
+  }
+}
+function greeter(person: Person) {
+  return `Hello . ${person.firstName}${person.lastName}`;
+}
+
+let user = { firstName: "first", lastName: "last" };
+document.body.innerHTML = greeter(user);
+
+let student = new Student("Jane", "m");
+console.log(student);
